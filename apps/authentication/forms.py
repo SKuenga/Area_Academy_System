47f690ee2a2d3ec5528d4f5b87061369dbd9183e
SKuenga <tradewithsacha@gmail.com>
2026-07-23 15:37:06 +0600

    Completed the backend and a skeleton frontend for log in page. We still need to do css styling for the log in page. Also we haven't finished the rerouting for the users based on their role. We just used HTTPRESPONSE for now.

---
+++ apps/authentication/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'})
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'})
+    )
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
-# Create your views here.
+from .forms import LoginForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.http import HttpResponse
+from .models import User
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            role = getattr(user, 'role', None)
+            if role == User.Role.SUPER_ADMIN:
+                return HttpResponse("Admin login successful")
+            elif role == User.Role.BRANCH_MANAGER:
+                return HttpResponse("Branch Manager login successful")
+            else:
+                return HttpResponse("Login successful")
+    else:
+        form = LoginForm()
+    return render(request, 'authentication/login.html', {'form': form})
