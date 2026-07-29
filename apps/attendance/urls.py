0301d44dc68e2d1ea4178e0d248bff5d48761daa
SKuenga <tradewithsacha@gmail.com>
2026-07-29 22:40:36 +0600

    Added branch_detail view and configured the urls.py

--- apps/attendance/urls.py
+++ apps/attendance/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import SuperAdminDashboard
+from .views import SuperAdminDashboard, branch_detail
 urlpatterns = [
-    path('admin-dashboard/', SuperAdminDashboard.as_view(), name="super_admin_dashboard")
+    path('admin-dashboard/', SuperAdminDashboard.as_view(), name="super_admin_dashboard"),
+    path('branch/<int:pk>/', branch_detail, name='branch_detail'),
 ]
--- apps/attendance/views.py
+++ apps/attendance/views.py
@@ -2,6 +2,8 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from apps.attendance.services.dashboard import get_branch_summary
+from .models import Branch
+
 class SuperAdminDashboard(TemplateView):
     template_name = 'attendance/admin_dashboard.html'
     def get_context_data(self, **kwargs):
@@ -13,4 +15,9 @@ class SuperAdminDashboard(TemplateView):
         context["total_present"]  = sum(b["present"] for b in branches)
         context["total_absent"]   = sum(b["absent"] for b in branches)
         return context
-        
+
+from django.shortcuts import get_object_or_404, render
+
+def branch_detail(request, pk):
+    branch = get_object_or_404(Branch, pk=pk)
+    return render(request, 'attendance/branch_detail.html', {'branch': branch})
