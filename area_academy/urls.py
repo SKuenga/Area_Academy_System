7ad2839636f0227be798876e04280b99e55b7499
SKuenga <tradewithsacha@gmail.com>
2026-08-12 21:04:22 +0600

    Got the redirect view to auth/ what directs toward logging in page.

--- area_academy/urls.py
+++ area_academy/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('apps.authentication.urls')),
+    path('', RedirectView.as_view(url='/auth/', permanent=True)),
     path('attendance/', include('apps.attendance.urls'))
 ]
